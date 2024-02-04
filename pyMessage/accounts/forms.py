0fa3e8a5360f74f17d77e5fba87afcbfa055af4a
thevlad0 <the_vlad0@icloud.com>
2024-02-04 18:03:32 +0200

    add user profile pic

--- pyMessage/accounts/forms.py
+++ pyMessage/accounts/forms.py
@@ -4,12 +4,13 @@ from phonenumber_field.formfields import PhoneNumberField
 from .models import MyUser
 
 class MyUserCreationForm(UserCreationForm):
-    first_name = forms.CharField(label=('Name:'), max_length=25, required=True,
-                                 widget=forms.TextInput(attrs={'class': 'textBox'}))
+    first_name = forms.CharField(label=('First Name:'), max_length=25, required=True,
+                                 widget=forms.TextInput(attrs={'class': 'textBox',
+                                                               'autofocus': True,}))
     last_name = forms.CharField(label=('Last Name:'), max_length=25, required=True, 
-                                widget=forms.TextInput(attrs={'class': 'textBox'}))
+                                widget=forms.TextInput(attrs={'class': 'textBox',}))
     phone = PhoneNumberField(label=('Phone:'), region='BG', required=True,
-                             widget=forms.TextInput(attrs={'class': 'textBox'}))
+                             widget=forms.TextInput(attrs={'class': 'textBox',}))
     email = forms.EmailField(label=('Email:'), max_length=50, required=False,
                              widget=forms.EmailInput(attrs={'class': 'textBox'}))
     password1 = forms.CharField(label=('Password:'), max_length=30, required=True, 
@@ -28,14 +29,12 @@ class MyUserChangeForm(UserChangeForm):
         fields = ('first_name', 'last_name', 'phone', 'email')
 
 class MyUserLoginForm(AuthenticationForm):
-    username = forms.CharField(label=('Enter your email or phone number:'), max_length=50, required=True,
-                               widget=forms.TextInput(attrs={'class': 'textBox', 
+    username = forms.CharField(label='Phone/Email:', max_length=50, required=True,
+                               widget=forms.TextInput(attrs={'class': 'textBox',
                                                              'placeholder': 'Phone/Email',
                                                              'autofocus': True
-                                                             }
-                                                      ))
-    password = forms.CharField(label=('Enter your password:'), max_length=30, required=True,
+                                                             }))
+    password = forms.CharField(label='Password:', max_length=30, required=True,
                                widget=forms.PasswordInput(attrs={'class': 'textBox',
                                                                  'placeholder': 'Password',
-                                                                }
-                                                          ))
+                                                                }))
--- pyMessage/accounts/models.py
+++ pyMessage/accounts/models.py
@@ -9,11 +9,20 @@ class MyUser(AbstractUser):
     username = None
     phone = PhoneNumberField(('phone'), region='BG', unique=True)
     email = models.EmailField(('email address'), unique=True)
+    
+    profile_picture = models.ImageField(upload_to='profile_pictures/', default='profile_pictures/default.png')
 
     USERNAME_FIELD = 'phone'
     REQUIRED_FIELDS = ('first_name', 'last_name', 'password')
 
     objects = MyUserManager()
+    
+    def get_data(self):
+        return {
+            'name': self.first_name + ' ' + self.last_name,
+            'username': self.phone if self.phone is not None else self.email,
+            'picture' : self.profile_picture.url
+        }
     
     def __str__(self):
         return str(self.phone)
